aee9b13e617cd4790b0036e87127c381c38d5880
park0923 <wndgks6249@gmail.com>
2022-09-16 10:36:17 +0900

    헤더 수정

--- front/src/Web/Header.js
+++ front/src/Web/Header.js
@@ -7,8 +7,7 @@ const Header = () => {
             <Link to={'/'} className='logo font-sebang-gothic font-bold'>Sinabro</Link>                    
             <ul>
                 <li><Link to={'user/signin'} className='font-sebang-gothic font-bold'>Sign In</Link></li>
-                <li><Link to={'user/signup'} className='font-sebang-gothic font-bold'>Sign Up</Link></li>
-                <li><Link to={'/UserDashboard'} className='font-sebang-gothic font-bold'>TopBar</Link></li>
+                <li><Link to={'user/signup'} className='font-sebang-gothic font-bold'>Sign Up</Link></li>                
             </ul>
         </header>
     );
